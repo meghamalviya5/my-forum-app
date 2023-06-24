26c838baec0fbb5745d7fd566eb02da876567a09
meghamalviya5 <megha.malviya444@gmail.com>
2023-06-24 22:30:49 +0530

    changes -

--- src/reducers/ForumReducer.jsx
+++ src/reducers/ForumReducer.jsx
@@ -2,6 +2,20 @@ export const forumreducer = (state, action) => {
   switch (action.type) {
     case "UPDATE_FORUM_DATA":
       return { ...state, allForumData: action.payload };
+
+    case "INCREMENT_VOTE":
+      const user = state.allForumData.find((user) => user.postId === postId);
+      if (user) {
+        user.upvotes = user.upvotes + 1;
+      }
+      return { ...state, allForumData: action.payload };
+
+    case "DECREMENT_VOTE":
+      const userD = state.allForumData.find((user) => user.postId === postId);
+      if (user) {
+        user.upvotes = user.upvotes - 1;
+      }
+      return { ...state, allForumData: action.payload };
     default:
       return { state };
   }
